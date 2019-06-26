ecfa09e99961b3d9c3ff6fc8a43df1af761055d1
Nahid71 <nahidjahid8698@gmail.com>
2019-06-27 03:22:14 +0600

    click in sign in button modify

--- github_test_with_testcafe/Test_One/github_login_test.js
+++ github_test_with_testcafe/Test_One/github_login_test.js
@@ -2,7 +2,7 @@ import { HomePage } from './homepage';
 import { nahidhassan } from './role'
 
 
-fixture `GitHub Tests`
+fixture `GitHub login Tests`
     // starting page.
     .page `https://github.com`;
 
@@ -10,9 +10,12 @@ const page = new HomePage();
 
 test('login as Nahid71', async t => {
     await t
+
+         // Click in sign in button
+         .click(page.signInButton)
          // Assinging a user.
         .useRole(nahidhassan)
         // checking for successfull login.
         .expect(page.dropdownHeader.textContent).contains('Nahid71');
-
+          console.log("Test pass successfull")
 });
--- github_test_with_testcafe/Test_One/homepage.js
+++ github_test_with_testcafe/Test_One/homepage.js
@@ -12,5 +12,6 @@ export class HomePage {
     constructor () {
 
         this.dropdownHeader = Selector('.header-nav-current-user');
+        this.signInButton = Selector('.HeaderMenu-link').nth(5);
     }
 }
